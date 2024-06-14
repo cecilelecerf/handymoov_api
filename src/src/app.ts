a07239d58f1b04d76020f167be55bede0d84b489
cecilelecerf <c.lecerf.cecile@gmail.com>
2024-06-14 09:21:16 +0200

    cors

--- src/src/app.ts
+++ src/src/app.ts
@@ -1,8 +1,9 @@
 import connect from "./utils/connect";
 import createServer from "./utils/server";
-
+import dotenv from "dotenv";
 const port = 3005;
 
+dotenv.config();
 const app = createServer();
 
 app.listen(port, async () => {
